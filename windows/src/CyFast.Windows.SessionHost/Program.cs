d4bd6d3fb1c93648aef7205aa18d132b4438ebeb
ab77913 <ananta.basudevmohanty@cyient.com>
2026-08-19 12:06:46 +0530

    fix(windows): dispose session runtime services cleanly

--- windows/src/CyFast.Windows.SessionHost/Program.cs
+++ windows/src/CyFast.Windows.SessionHost/Program.cs
@@ -7,8 +7,12 @@ var pipeName = args.FirstOrDefault(arg => arg.StartsWith("--pipe=", StringCompar
     ?? "CyFast.Windows.SessionHost";
 using var loggerFactory = LoggerFactory.Create(builder => builder.AddConsole());
 using var automation = new CyFast.Windows.Uia.FlaUi3Backend();
-var host = new SessionHost(automation);
+using var host = new SessionHost(automation);
 var server = new NamedPipeIpcServer(loggerFactory.CreateLogger<NamedPipeIpcServer>());
 using var shutdown = new CancellationTokenSource();
-Console.CancelKeyPress += (_, eventArgs) => { eventArgs.Cancel = true; shutdown.Cancel(); };
+Console.CancelKeyPress += (_, eventArgs) =>
+{
+    eventArgs.Cancel = true;
+    shutdown.Cancel();
+};
 await server.ServeLoopAsync(pipeName, host.HandleAsync, shutdown.Token);

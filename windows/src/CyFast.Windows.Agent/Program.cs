a0e6351d5fd57ba5bd2512e19fc630239c55d2d1
ab77913 <ananta.basudevmohanty@cyient.com>
2026-08-19 12:06:33 +0530

    fix(windows): persist unsent command results across reconnects

--- windows/src/CyFast.Windows.Agent/Program.cs
+++ windows/src/CyFast.Windows.Agent/Program.cs
@@ -5,7 +5,7 @@ var builder = Host.CreateApplicationBuilder(args);
 builder.Services.Configure<AgentOptions>(builder.Configuration.GetSection("Agent"));
 builder.Services.AddSingleton<IIdentityStore, DpapiIdentityStore>();
 builder.Services.AddSingleton<CommandValidator>();
-builder.Services.AddSingleton<IResultSpool, InMemoryResultSpool>();
+builder.Services.AddSingleton<IResultSpool, FileResultSpool>();
 builder.Services.AddSingleton<NamedPipeIpcClient>();
 builder.Services.AddHttpClient<EnrollmentClient>();
 builder.Services.AddSingleton<GatewayConnection>();
